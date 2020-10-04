acbed1400c490b6658c4221bd88dc7227e82630f
Bernard Arhia <55264725+bernardarhia@users.noreply.github.com>
2020-10-04 20:42:53 +0000

    updated the slider.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Slider from "./Slider";
-import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
